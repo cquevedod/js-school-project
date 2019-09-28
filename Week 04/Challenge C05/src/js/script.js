e9e21351e5902a5cc8f76afe83c0f0ac30885810
Carlos Quevedo <carlosquedid@gmail.com>
2019-09-28 15:47:54 -0500

    file modified to just render de function renderBooks from renderBooks.js

--- Week 04/Challenge C05/src/js/script.js
+++ Week 04/Challenge C05/src/js/script.js
@@ -1,23 +1,5 @@
 import { renderBooks } from '../../renderBook.js'
 renderBooks();
 
-var textOverImages = document.getElementsByClassName("onClickTextOverImage");
-var previousTextOverImage;
-for (var i = 0; i < textOverImages.length; i++) {
-  textOverImages[i].onclick = function() {
-    var classes = this.classList;
-    if (classes.contains("show")) {
-      classes.remove("show");
-    } else {
-      if (previousTextOverImage != null)
-        previousTextOverImage.classList.remove("show");
-      previousTextOverImage = this;
-      classes.add("show");
-    }
-  }
-}
 
-function stopPropagation(event){
-  event.stopPropagation();
-}
 
