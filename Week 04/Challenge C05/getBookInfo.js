9f87c3e618c9c0a8e99c9c7bbc89c1fe97d93e38
Carlos Quevedo <carlosquedid@gmail.com>
2019-09-29 23:52:58 -0500

    script modified a little

--- Week 04/Challenge C05/getBookInfo.js
+++ Week 04/Challenge C05/getBookInfo.js
@@ -1,53 +1,43 @@
-const  fs =  require('fs');
-const fetch = require('node-fetch');
+const fs = require("fs");
+const fetch = require("node-fetch");
 
+const apiurl = "https://www.googleapis.com/books/v1/volumes?q=subject:";
 
-const apiurl = 'https://www.googleapis.com/books/v1/volumes?q=subject:';
+let topics = ["FANTASY", "DRAMA"];
 
-
- let topics = ['FANTASY', 'DRAMA'];
-
-function concatenate (url, ...themes) {
-  return themes.map(element => 
-     `${url}${element}`);
+function concatenate(url, ...themes) {
+  return themes.map(element => `${url}${element}`);
 }
 
-let urls = concatenate(apiurl,...topics);
+let urls = concatenate(apiurl, ...topics);
 let bookSet = {
   bookShelf: []
 };
 
 for (let url of urls) {
   fetch(url)
-  .then((resp) => resp.json())
-  .then(data => {
-    
-    data.items.forEach( book => {
-      if (!data.items) return;
-
-      bookSet.bookShelf.push({
-      title: book.volumeInfo.title,
-      publishedDate: book.volumeInfo.publishedDate,
-      authors: book.volumeInfo.authors,
-      pageCount: book.volumeInfo.pageCount,
-      description: book.volumeInfo.description,
-      averageRating: book.volumeInfo.averageRating,
-      thumbnail: book.volumeInfo.imageLinks.thumbnail,
-      isbn: book.volumeInfo.industryIdentifiers
+    .then(resp => resp.json())
+    .then(data => {
+      data.items.forEach(book => {
+        if (!data.items) return;
+        bookSet.bookShelf.push({
+          title: book.volumeInfo.title,
+          publishedDate: book.volumeInfo.publishedDate.substr(0, 4),
+          authors: book.volumeInfo.authors,
+          pageCount: book.volumeInfo.pageCount,
+          description: book.volumeInfo.description,
+          averageRating: book.volumeInfo.averageRating,
+          thumbnail: book.volumeInfo.imageLinks.thumbnail,
+          isbn: book.volumeInfo.industryIdentifiers
+        });
+        let info = JSON.stringify(bookSet, null, 2);
+        fs.writeFileSync("./books.json", info, err => {
+          if (err) throw err;
+          console.log("Data written into a file");
+        });
+      });
     })
-    let info = JSON.stringify(bookSet, null, 2);
-    fs.writeFileSync('./books.json', info,(err) => {
-      if (err) throw err;
-      console.log('Data written into a file');
-  });
+    .catch(err => {
+      console.log(err);
     });
-  })
-  .catch(err => {
-    console.log(err)
-  })
 }
-
-
-
-
- 
