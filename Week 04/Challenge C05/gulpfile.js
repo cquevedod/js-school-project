221fe870ef954dd7766f34091fbc7f25cade7f73
Carlos Quevedo <carlosquedid@gmail.com>
2019-09-29 23:53:45 -0500

    task for compile scss files into css was added

--- Week 04/Challenge C05/gulpfile.js
+++ Week 04/Challenge C05/gulpfile.js
@@ -1,25 +1,40 @@
-const gulp = require('gulp');
-const browserify = require('browserify');
-const source = require('vinyl-source-stream');
-const buffer = require('vinyl-buffer');
-const babelify = require('babelify');
-const rename = require('gulp-rename');
+const gulp = require("gulp");
+const browserify = require("browserify");
+const source = require("vinyl-source-stream");
+const buffer = require("vinyl-buffer");
+const babelify = require("babelify");
+const rename = require("gulp-rename");
+const uglify = require("gulp-uglify");
+const sass = require("gulp-sass");
+const sourcemaps = require("gulp-sourcemaps");
 
-gulp.task('js', async function () {
-    return browserify('./src/js/script.js')
-        .transform('babelify', {
-            presets: ['@babel/preset-env']
-        })
-        .bundle()
-        .pipe(source('app.js')) // Converts To Vinyl Stream
-        .pipe(buffer()) // Converts Vinyl Stream To Vinyl Buffer
-        .pipe(rename( {suffix: '.min'} ) )
-        // Gulp Plugins Here!
-        .pipe(gulp.dest('./dist/scripts'))
+gulp.task("js", async function() {
+  return browserify("./src/js/script.js")
+    .transform("babelify", {
+      presets: ["@babel/preset-env"]
+    })
+    .bundle()
+    .pipe(source("app.js"))
+    .pipe(buffer())
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(uglify())
+    .pipe(sourcemaps.write("./"))
+    .pipe(gulp.dest("./dist/scripts"));
 });
 
-gulp.task('watch', async () => {
-    gulp.watch('./src/js/script.js', gulp.series('js'));
+gulp.task("sass", async function() {
+  return gulp
+    .src("./src/styles/*.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("./dist/styles"));
 });
 
-gulp.task('default', gulp.parallel('js', 'watch'));
+gulp.task("watch", async () => {
+  gulp.watch("./src/js/script.js", gulp.series("js"));
+  gulp.watch("./src/styles/*.scss", gulp.series("sass"));
+});
+
+gulp.task("default", gulp.parallel("js", "sass", "watch"));
