9592746c56f9f85d80d321bf251840b70003ccff
Carlos Quevedo <carlosquedid@gmail.com>
2019-10-04 12:46:17 -0500

    Script to populate the Database was added

---
+++ Week 05/Challenge-C06/getData.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const mongoose = require("mongoose");
+const port = process.env.PORT || 3977;
+const fs = require("fs");
+const Books = require("./models/bookModel");
+
+mongoose
+  .connect("mongodb://localhost:27017/bookInfo", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log("connection succesful!"))
+  .catch(err => console.error("Error trying to connect", err));
+
+async function getDataFromJSON() {
+  const books = JSON.parse(fs.readFileSync("./books.json", "utf8"));
+  return books;
+}
+
+async function populateDB(req, res) {
+  getDataFromJSON().then(data => {
+    data.books.forEach(book => {
+      const newBook = new Books(book);
+      newBook.save();
+    });
+    res.send("db populated");
+  })
+  .catch(function(err) {
+    console.log("error: ", err);
+  });
+}
+
+module.exports = {
+  populateDB
+};
