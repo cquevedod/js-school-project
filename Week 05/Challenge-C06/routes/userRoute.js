eaf8089b0d7c70db80c7bb1b28cefb41a60c37ae
Carlos Quevedo <carlosquedid@gmail.com>
2019-10-05 02:50:40 -0500

    Change of name function of the register endpoint

--- Week 05/Challenge-C06/routes/userRoute.js
+++ Week 05/Challenge-C06/routes/userRoute.js
@@ -2,12 +2,11 @@
 
 const express = require("express");
 const UserController = require("../controllers/userController");
-// const data = require('../getData');
 
 let api = express.Router();
 let md_auth = require("../middlewares/auth");
 api.get("/testing-controller", md_auth.ensureAuth, UserController.tests);
-api.post("/register", UserController.saveUser);
+api.post("/register", UserController.register);
 api.post("/login", UserController.loginUser);
 
 module.exports = api;
