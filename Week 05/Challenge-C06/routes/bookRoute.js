f6ff2c9e86e27c39c5a4cdf3f009622107389604
Carlos Quevedo <carlosquedid@gmail.com>
2019-10-05 02:49:34 -0500

    route for return book endpoint was added

--- Week 05/Challenge-C06/routes/bookRoute.js
+++ Week 05/Challenge-C06/routes/bookRoute.js
@@ -6,8 +6,9 @@ const BookController = require("../controllers/bookController");
 let api = express.Router();
 let md_auth = require("../middlewares/auth");
 
-api.get("/book/:id", md_auth.ensureAuth,  BookController.getBook);
+api.get("/book/:id", md_auth.ensureAuth,  BookController.getBookById);
 api.get("/books/all", md_auth.ensureAuth, BookController.getAllBooksOrByBookshelf);
-api.get("/books/:bookShelf", md_auth.ensureAuth, BookController.getAllBooksOrByBookshelf); //filter books by Bookshelf
+api.get("/bookshelf/:bookShelf", md_auth.ensureAuth, BookController.getAllBooksOrByBookshelf); //filter books by Bookshelf
 api.get("/book/:id/lend", md_auth.ensureAuth, BookController.lendBook);
+api.get("/book/:id/return", md_auth.ensureAuth, BookController.returnBook);
 module.exports = api;
