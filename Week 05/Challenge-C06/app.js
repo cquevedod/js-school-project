a5c2e4ed9572b23b59bf1684a8cab00cd964c62b
Carlos Quevedo <carlosquedid@gmail.com>
2019-10-05 14:40:12 -0500

    Unuseful comments removed

--- Week 05/Challenge-C06/app.js
+++ Week 05/Challenge-C06/app.js
@@ -6,31 +6,22 @@ const bodyparser = require('body-parser');
 let app = express();
 const data = require('./getData');
 
-//load paths
+/*load paths*/
 const user_routes = require('./routes/userRoute');
 const book_routes = require('./routes/bookRoute')
 
 app.use(bodyparser.urlencoded({extended:false}));
 app.use(bodyparser.json());
 
-// set headers http
-
-// base paths (/api/...)
-
-//Middleware, before make request have 'api' adelante, 
-// Se carga la ruta que se tenga configurada.
 app.use('/api', user_routes); 
 app.use('/api', book_routes); 
 
 app.get('/test', (req, res) => {
-    res.status(200).send({message: 'Welcome to the test response'})
+    res.status(200).send({message: 'Test succesful'})
 });
-
-// const baseUrl = '/api/';
 
 app.get(`/api/populateDB`, (req, res) => {
   data.populateDB(req, res);
 });
-
 
 module.exports = app;
