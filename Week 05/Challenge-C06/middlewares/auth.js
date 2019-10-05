3c1ea15ac32caac3ec50b8bc056324aa55c48264
Carlos Quevedo <carlosquedid@gmail.com>
2019-10-05 02:48:35 -0500

    use var instead of let in line 22, because of the scope

--- Week 05/Challenge-C06/middlewares/auth.js
+++ Week 05/Challenge-C06/middlewares/auth.js
@@ -2,6 +2,7 @@
 
 const jwt = require('jwt-simple');
 const moment = require('moment');
+const msg = require("../controllers/statusMsg");
 let secret = 'one_second'; // secret key to hashear el objeto createToken
 
 // permite comprobar si los datos del token
@@ -9,29 +10,25 @@ let secret = 'one_second'; // secret key to hashear el objeto createToken
 // va a recibir todos los parametros, los valores que va a
 // recibir una petición HTTP
 exports.ensureAuth = function(req, res, next) {
-  // se recoge la autorización ahora, lo que venga por la request
   if (!req.headers.authorization) {
     return res
-      .status(403)
-      .send({ message: "You need authorization to do this action. Please login and get the Token" });
+      .status(401)
+      .send(msg.unAuthorized('You need authorization to do this action. Please login and get the Token'));
   }
 
   let token = req.headers.authorization.replace(/['"]+/g, ""); // remove comillas simples y dobles que pueda tener el string
 
   try {
-    var payload = jwt.decode(token, secret); // I use var here because of hoisting, if use let. The payload in the line 39 will be undefined
-    // if (!payload) {
-    //   return res.status(428).send({ message: "You need to login first!" });
-    // }
+    var payload = jwt.decode(token, secret); //use var here because of hoisting. if use let, the payload in the line 39 will be undefined
 
     console.log(payload);
 
     if (payload.exp <= moment().unix()) {
-      return res.status(401).send({ message: "Token has expirated" });
+      return res.status(401).send({ message: 'Token has expired' });
     }
   } catch (ex) {
     console.log(ex);
-    return res.status(401).send({ message: "Invalid Token" });
+    return res.status(401).send(msg.unAuthorized('Invalid authentication Token'));
   }
   console.log("Now...");
   // console.log(payload);
