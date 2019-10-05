7c66e26fe96049c3aa0ad802eb71f5f0ee0f7417
Carlos Quevedo <carlosquedid@gmail.com>
2019-10-05 14:47:29 -0500

    POST instead of GET in lines 12 and 13, lend and return book endpoints

--- Week 05/Challenge-C06/routes/bookRoute.js
+++ Week 05/Challenge-C06/routes/bookRoute.js
@@ -9,6 +9,6 @@ let md_auth = require("../middlewares/auth");
 api.get("/book/:id", md_auth.ensureAuth,  BookController.getBookById);
 api.get("/books/all", md_auth.ensureAuth, BookController.getAllBooksOrByBookshelf);
 api.get("/bookshelf/:bookShelf", md_auth.ensureAuth, BookController.getAllBooksOrByBookshelf); //filter books by Bookshelf
-api.get("/book/:id/lend", md_auth.ensureAuth, BookController.lendBook);
-api.get("/book/:id/return", md_auth.ensureAuth, BookController.returnBook);
+api.post("/book/:id/lend", md_auth.ensureAuth, BookController.lendBook);
+api.post("/book/:id/return", md_auth.ensureAuth, BookController.returnBook);
 module.exports = api;
