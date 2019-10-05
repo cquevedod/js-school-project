fd910b9234d075b30ac5751bc61223deefbcac45
Carlos Quevedo <carlosquedid@gmail.com>
2019-10-05 14:50:21 -0500

    Indentation improvement

--- Week 05/Challenge-C06/index.js
+++ Week 05/Challenge-C06/index.js
@@ -4,17 +4,16 @@ const mongoose = require('mongoose');
 const app = require('./app');
 const port = process.env.PORT || 3977;
 
-
 mongoose.connect('mongodb://localhost:27017/bookInfo',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false 
-    })
-    .then(() => console.log('connection to database succesful!'))
-    .catch (err => console.error('Error trying to connect to Mongo DB', err))
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false 
+})
+.then(() => console.log('Connection to database succesful!'))
+.catch (err => console.error('Error trying to connect to Mongo DB', err))
 
-    app.listen(port, function() {
-        console.log(`Bookshelf server listening at http://localhost:${port}`);
-    });
+app.listen(port, function() {
+  console.log(`Server listening at http://localhost:${port}`);
+});
 
   
