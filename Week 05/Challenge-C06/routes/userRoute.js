85b17b889789bf774692626309402a2f6465243e
Carlos Quevedo <carlosquedid@gmail.com>
2019-10-05 14:47:53 -0500

    general improvements in code

--- Week 05/Challenge-C06/routes/userRoute.js
+++ Week 05/Challenge-C06/routes/userRoute.js
@@ -4,8 +4,6 @@ const express = require("express");
 const UserController = require("../controllers/userController");
 
 let api = express.Router();
-let md_auth = require("../middlewares/auth");
-api.get("/testing-controller", md_auth.ensureAuth, UserController.tests);
 api.post("/register", UserController.register);
 api.post("/login", UserController.loginUser);
 
