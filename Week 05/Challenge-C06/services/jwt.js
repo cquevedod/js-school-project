4e92d0165bae50de75167469fb7109f6d71edb5e
Carlos Quevedo <carlosquedid@gmail.com>
2019-10-05 14:49:03 -0500

    Unuseful comments removed and indentation improvement

--- Week 05/Challenge-C06/services/jwt.js
+++ Week 05/Challenge-C06/services/jwt.js
@@ -1,35 +1,22 @@
 'use strict';
 const jwt = require('jwt-simple');
-const moment = require('moment');
-let secret = 'one_second'; // secret key to hashear el objeto createToken
+/* used to have the creation date and exp date of the token */
+const moment = require('moment'); 
+/* secret key to hash the createToken Object */
+let secret = 'one_second'; 
 
-// Moment para hacer dentro del payload
-//tener la fecha de creación del token (o payload)
-// y también la fecha de expiración del token
-// si ha exprirado, es necesario volver a loguearse
-
-// El objeto de usuario lo va a guardar
-// dentro de la codificación dl token,
-// como un hash
 exports.createToken = function(user) {
-  // datos a codificar
   let payload = {
-    sub: user._id, // save the user's id
+    sub: user._id, 
     name: user.name,
     surname: user.surname,
     email: user.email,
     role: user.role,
-    // las fechas se colocan en formato unix para poder
-    //compararlas
-    iat: moment().unix(), // la fecha del token
-    exp: moment().add(30, 'days').unix // que expire cada cinco días
+    /*Dates in unit format in order to compare each other*/
+    iat: moment().unix(), //token date
+    exp: moment().add(1, 'days').unix //expiration time
   };
-  // reurn the encoded token, se cifra el payload (que es el objeto del usuario que se va a identificar: son los datos que van a estar dentro del token guardado) y se
-  // Utiliza una clave secreta para hacer el hash que el token genera
+ 
   return jwt.encode(payload, secret);
 };
 
-// Puedes hacer otros metodos que se te ocurran
-// el siguiente paso es crear el token y poder utilizar este servicio
-
-// REALIZAR EL SERVICIO PARA LOS LIBROS TAMBIÉN
