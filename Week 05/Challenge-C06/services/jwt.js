4d57deb4110d7653ed32aa54b6804158e66e3e44
Carlos Quevedo <carlosquedid@gmail.com>
2019-10-06 18:09:00 -0500

    a few comments were added

--- Week 05/Challenge-C06/services/jwt.js
+++ Week 05/Challenge-C06/services/jwt.js
@@ -1,6 +1,7 @@
 'use strict';
 const jwt = require('jwt-simple');
-/* used to have the creation date and exp date of the token */
+
+/* used to obtain the creation date and exp date of the token */
 const moment = require('moment'); 
 /* secret key to hash the createToken Object */
 let secret = 'one_second'; 
@@ -12,9 +13,10 @@ exports.createToken = function(user) {
     surname: user.surname,
     email: user.email,
     role: user.role,
+
     /*Dates in unit format in order to compare each other*/
     iat: moment().unix(), //token date
-    exp: moment().add(1, 'days').unix //expiration time
+    exp: moment().add(4, 'hours').unix //expiration time
   };
  
   return jwt.encode(payload, secret);
