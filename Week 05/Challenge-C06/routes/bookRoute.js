24dbacd43b23f08f161f045553f609640350de98
Carlos Quevedo <carlosquedid@gmail.com>
2019-10-06 17:55:33 -0500

    routes modified. The return route was deleted, instead of it, a query param was added in bookController.js line 24

--- Week 05/Challenge-C06/routes/bookRoute.js
+++ Week 05/Challenge-C06/routes/bookRoute.js
@@ -6,9 +6,7 @@ const BookController = require("../controllers/bookController");
 let api = express.Router();
 let md_auth = require("../middlewares/auth");
 
-api.get("/book/:id", md_auth.ensureAuth,  BookController.getBookById);
-api.get("/books/all", md_auth.ensureAuth, BookController.getAllBooksOrByBookshelf);
-api.get("/bookshelf/:bookShelf", md_auth.ensureAuth, BookController.getAllBooksOrByBookshelf); //filter books by Bookshelf
-api.post("/book/:id/lend", md_auth.ensureAuth, BookController.lendBook);
-api.post("/book/:id/return", md_auth.ensureAuth, BookController.returnBook);
+api.get("/books/:id", md_auth.ensureAuth,  BookController.getBookById);
+api.get("/books", md_auth.ensureAuth, BookController.getAllBooksOrByBookshelf);
+api.post("/books/:id/lend", md_auth.ensureAuth, BookController.lendBook);
 module.exports = api;
