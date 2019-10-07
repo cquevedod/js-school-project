bb5167fa93f46533c450617ce306312a9012754f
Carlos Quevedo <carlosquedid@gmail.com>
2019-10-06 23:57:30 -0500

    Indentation improvements

--- Week 05/Challenge-C06/middlewares/auth.js
+++ Week 05/Challenge-C06/middlewares/auth.js
@@ -3,24 +3,20 @@
 const jwt = require('jwt-simple');
 const moment = require('moment');
 const msg = require("../controllers/statusMsg");
-let secret = 'one_second'; // secret key to hashear createToken object
+let secret = 'one_second'; // secret key to hash createToken object
 
-
-// ensure if the input data is correct
 exports.ensureAuth = function(req, res, next) {
   if (!req.headers.authorization) {
     return res
       .status(401)
-      .send(msg.unAuthorized('You need authorization to do this action. Please login and get the Token'));
+      .send(msg
+      .unAuthorized('You need authorization to do this action. Please login and get the Token'));
   }
 
-  // remove "" 
   let token = req.headers.authorization.replace(/['"]+/g, ""); 
-
   try {
     //var used here because of hoisting. if use let, the payload in the line 39 will be undefined
     var payload = jwt.decode(token, secret); 
-
     if (payload.exp <= moment().unix()) {
       return res.status(401).send({ message: 'Token has expired' });
     }
