77fec47f34c9fdd7b759532d5457cf5e254f4a90
Carlos Quevedo <carlosquedid@gmail.com>
2019-10-12 00:06:07 -0500

    header and search components rendered

--- Week 06/Challenge C07/frontend/src/components/header/Header.js
+++ Week 06/Challenge C07/frontend/src/components/header/Header.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import logo from '../../assets/img/logo.svg';
 import avatar from '../../assets/img/avatar.png';
 import './Header.css';
+import Search from '../search/Search';
 
 export default class Header extends Component {
     render() {
@@ -16,20 +17,7 @@ export default class Header extends Component {
                     </div>
                     <div class="nav-center-container">
                         <a href="#" id="book-box"><span id="bookshelf">Bookshelf</span></a>
-                        <div class="search-container">
-                            <form action="#">
-                                <label for="search-field">
-                                    <i class="fa fa-search search-icon"></i>
-                                </label>
-                                <input
-                                    class="search-box"
-                                    type="text"
-                                    placeholder="Search..."
-                                    name="search"
-                                    required=""
-                                />
-                            </form>
-                        </div>
+                       <Search/>
                     </div>
                     
                     <div class="login">
---
+++ Week 06/Challenge C07/frontend/src/components/search/Search.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import './Search.css';
+
+export default class Search extends Component {
+    render() {
+        return (
+            <div>
+               <div class="search-container">
+                            <form action="#">
+                                <label for="search-field">
+                                    <i class="fa fa-search search-icon"></i>
+                                </label>
+                                <input
+                                    class="search-box"
+                                    type="text"
+                                    placeholder="Search..."
+                                    name="search"
+                                    required=""
+                                />
+                            </form>
+                        </div>
+            </div>
+        )
+    }
+}
