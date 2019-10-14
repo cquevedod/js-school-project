a892b3876a5b850c3023d6b013acbadb20583ca1
Carlos Quevedo <carlosquedid@gmail.com>
2019-10-14 11:03:12 -0500

    Info on click over book's covers was added. Star rating fixed

--- Week 06/Challenge C07/frontend/src/components/book/Book.jsx
+++ Week 06/Challenge C07/frontend/src/components/book/Book.jsx
@@ -7,16 +7,16 @@ import bookmarkImg from '../../assets/img/rate-book/bookmark.png';
 
 
 export default class Book extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          bookmarked: false,
-          liked: false,
-          userRating: 0,
-          openedSeethroughOptions: false,
-          openedDetails: false,
-          openedLendOptions: false,
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookmarked: false,
+      liked: false,
+      userRating: 0,
+      openedSeethroughOptions: false,
+      openedDetails: false,
+      openedLendOptions: false,
+    };
 
     this.clickBookmarkHandler = this.clickBookmarkHandler.bind(this);
     this.clickDescriptionHandler = this.clickDescriptionHandler.bind(this);
@@ -24,162 +24,246 @@ export default class Book extends Component {
     this.clickRatingHandler = this.clickRatingHandler.bind(this);
     this.clickSeethroughHandler = this.clickSeethroughHandler.bind(this);
     this.clickLendButtonHandler = this.clickLendButtonHandler.bind(this);
-    }
+  }
 
-    clickLendButtonHandler(event) {
-        event.stopPropagation();
-        const { openedLendOptions } = this.state;
-        this.setState({
-          openedLendOptions: !openedLendOptions,
-        });
-      }
-    
-      clickSeethroughHandler(event) {
-        event.stopPropagation();
-        const { openedSeethroughOptions } = this.state;
-        this.setState({
-          openedSeethroughOptions: !openedSeethroughOptions,
-        });
-      }
-    
-      clickBookmarkHandler(event) {
-        event.stopPropagation();
-        const { bookmarked } = this.state;
-        this.setState({
-          bookmarked: !bookmarked,
-        });
-      }
-    
-      clickLikeHandler(event) {
-        event.stopPropagation();
-        const { liked } = this.state;
-        this.setState({
-          liked: !liked,
-        });
-      }
-    
-      clickDescriptionHandler(event) {
-        event.stopPropagation();
-        const { openedDetails } = this.state;
-        this.setState({
-          openedDetails: !openedDetails,
-        });
-      }
-    
-      clickRatingHandler(event, rating) {
-        event.stopPropagation();
-        this.setState({
-          userRating: rating,
-        });
-      }
+  clickLendButtonHandler(event) {
+    event.stopPropagation();
+    const { openedLendOptions } = this.state;
+    this.setState({
+      openedLendOptions: !openedLendOptions,
+    });
+  }
 
-    render() {
-        const {
-            id,
-            title,
-            publishedDate,
-            author,
-            pageCount,
-            description,
-            averageRating,
-            thumbnail,
-          } = this.props;
-      
-          const {
-            bookmarked,
-            liked,
-            userRating,
-            openedDetails,
-            openedSeethroughOptions,
-            openedLendOptions,
-          } = this.state;
+  clickSeethroughHandler(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    const { openedSeethroughOptions } = this.state;
+    this.setState({
+      openedSeethroughOptions: !openedSeethroughOptions,
+    });
+  }
 
-          const styles = {
-            backgroundImage: `url(${thumbnail})`
-        };
-      
-        return (
-             
-            <div className="column">
+  clickBookmarkHandler(event) {
+    event.stopPropagation();
+    const { bookmarked } = this.state;
+    this.setState({
+      bookmarked: !bookmarked,
+    });
+  }
+
+  clickLikeHandler(event) {
+    event.stopPropagation();
+    const { liked } = this.state;
+    this.setState({
+      liked: !liked,
+    });
+  }
+
+  clickDescriptionHandler(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    const { openedDetails } = this.state;
+    this.setState({
+      openedDetails: !openedDetails,
+    });
+  }
+
+  clickRatingHandler(event, rating) {
+    event.stopPropagation();
+    this.setState({
+      userRating: rating,
+    });
+  }
+
+  render() {
+    const {
+      id,
+      title,
+      publishedDate,
+      author,
+      pageCount,
+      description,
+      averageRating,
+      thumbnail,
+    } = this.props;
+
+    const {
+      bookmarked,
+      liked,
+      userRating,
+      openedDetails,
+      openedSeethroughOptions,
+      openedLendOptions,
+    } = this.state;
+
+    const styles = {
+      backgroundImage: `url(${thumbnail})`
+    };
+
+    return (
+
+      <div className="column">
+        <div
+          className="label-btn"
+          onClick={this.clickSeethroughHandler}
+          onKeyDown={this.clickSeethroughHandler}
+          tabIndex="0"
+          role="button"
+        >
+          <div className="cover">
+            <div className="onClickTextOverImage show" style={styles}>
+              <div className={`${openedSeethroughOptions ? 'book-cover-seethrough-selected' : 'book-cover-seethrough-unselected'}`}>
+                <div className="heart-button-cell">
+                  <div
+                    className="heart-button-wrapper"
+                    onClick={this.clickLikeHandler}
+                    onKeyDown={this.clickLikeHandler}
+                    role="button"
+                    tabIndex="0"
+                  >
+                    <i className={`${liked ? 'fa' : 'far'} fa-heart`} />
+                  </div>
+                </div>
+
+                <div className="bookmark-button-cell">
                 <div
-                    className="label-btn"
-                    onClick={this.clickSeethroughHandler}
-                    onKeyDown={this.clickSeethroughHandler}
+                  className="bookmark-button-wrapper"
+                  onClick={this.clickBookmarkHandler}
+                  onKeyDown={this.clickBookmarkHandler}
+                  role="button"
+                  tabIndex="0"
+                >
+                  <i className={`${bookmarked ? 'fa' : 'far'} fa-bookmark`} />
+                </div>
+              </div>
+              <div className="description-button-cell">
+                <div
+                  className="description-button-wrapper"
+                  onClick={this.clickDescriptionHandler}
+                  onKeyDown={this.clickDescriptionHandler}
+                  role="button"
+                  tabIndex="0"
+                >
+                  <i className="fa fa-book-open" />
+                </div>
+              </div>
+              <div className="seethrough-title-rating">
+                <div className="seethrough-title">
+                    RATE THIS BOOK
+                </div>
+                <div className="seethrough-rating">
+                  <i
+                    className={`${userRating >= 1 ? 'fa' : 'far'} fa-star`}
+                    onClick={(event) => this.clickRatingHandler(event, 1)}
+                    onKeyDown={(event) => this.clickRatingHandler(event, 1)}
                     tabIndex="0"
                     role="button"
-                >
-                    <div className="cover">
-                        <div className="onClickTextOverImage" style={styles}>
-                            <div className="info-over-cover">
-                                <img src={heartImage} className="icon-up-left" />
-                                <img src={openedBookImg} className="icon-center" />
-                                <img src={bookmarkImg} className="icon-up-right" />
-                                <p className="rating-book">rate this book</p>
-                                <div className="star-rating-book rate">
-                                    {`${userRating}`}
-                                </div>
-                            </div>
-                            <i className="fas fa-caret-left arrow"></i>
-                            <div id="bookModalInfo" className="bookModalInfo">
-                                <div id="modalHeader">
-                                    <div id="titleAndYear">
-                                        <p className="titleModal" style={{ margin: '1% 0' }}>{title}</p>
-                                        <p className="yearModal">{publishedDate}</p>
-                                    </div>
-                                    <p className="authorModal" style={{ margin: '1% 0' }}>
-                                        Novel by<span id="color-word"> {author}</span>
-                                    </p>
-                                    <p className="pageCount">{pageCount} pages</p>
-                                </div>
-                                <p id="summaryTitle">SUMMARY</p>
-                                <article id="summary" style={{ margin: '5% 0' }}>
-                                    {description}
-                                </article>
-                                <p id="ratingTitle" style={{ margin: '1% 0' }}>RATING</p>
-                                <div className="star-rating rate">
-                                    {averageRating}
-                                </div>
-                                <p style={{ margin: '6% 0' }}></p>
-                                <p className="recommend" style={{ margin: '1% 0' }}>RECOMMENDED BY</p>
-                                <div className="recommended">
-                                    <img src="img/recommended/user1.png" />
-                                    <img src="img/recommended/user2.png" />
-                                    <img src="img/recommended/user3.png" />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="cover-info">
-                            <p className="book-title">{title}</p>
-                            <p className="book-author">{author}</p>
-                            <div className="star-rating rate">
-                                {averageRating}
-                            </div>
-                        </div>
-                    </div>
+                    aria-label="Rate 1 star"
+                  />
+                  <i
+                    className={`${userRating >= 2 ? 'fa' : 'far'} fa-star`}
+                    onClick={(event) => this.clickRatingHandler(event, 2)}
+                    onKeyDown={(event) => this.clickRatingHandler(event, 2)}
+                    tabIndex="0"
+                    role="button"
+                    aria-label="Rate 2 stars"
+                  />
+                  <i
+                    className={`${userRating >= 3 ? 'fa' : 'far'} fa-star`}
+                    onClick={(event) => this.clickRatingHandler(event, 3)}
+                    onKeyDown={(event) => this.clickRatingHandler(event, 3)}
+                    tabIndex="0"
+                    role="button"
+                    aria-label="Rate 3 stars"
+                  />
+                  <i
+                    className={`${userRating >= 4 ? 'fa' : 'far'} fa-star`}
+                    onClick={(event) => this.clickRatingHandler(event, 4)}
+                    onKeyDown={(event) => this.clickRatingHandler(event, 4)}
+                    tabIndex="0"
+                    role="button"
+                    aria-label="Rate 4 stars"
+                  />
+                  <i
+                    className={`${userRating >= 5 ? 'fa' : 'far'} fa-star`}
+                    onClick={(event) => this.clickRatingHandler(event, 5)}
+                    onKeyDown={(event) => this.clickRatingHandler(event, 5)}
+                    tabIndex="0"
+                    role="button"
+                    aria-label="Rate 5 stars"
+                  />
                 </div>
+              </div>
+                <i className="fas fa-caret-left arrow"></i>
+                <div id="bookModalInfo" className="bookModalInfo" >
+                  <div id="modalHeader">
+                    <div id="titleAndYear">
+                      <p className="titleModal" style={{ margin: '1% 0' }}>{title}</p>
+                      <p className="yearModal">{publishedDate}</p>
+                    </div>
+                    <p className="authorModal" style={{ margin: '1% 0' }}>
+                      Novel by<span id="color-word"> {author}</span>
+                    </p>
+                    <p className="pageCount">{pageCount} pages</p>
+                  </div>
+                  <p id="summaryTitle">SUMMARY</p>
+                  <article id="summary" style={{ margin: '5% 0' }}>
+                    {description}
+                  </article>
+                  <p id="ratingTitle" style={{ margin: '1% 0' }}>RATING</p>
+                  <div className="star-rating rate">
+                  <i className={`${averageRating >= 1 ? 'fa' : 'far'} fa-star`} />
+          <i className={`${averageRating >= 2 ? 'fa' : 'far'} fa-star`} />
+          <i className={`${averageRating >= 3 ? 'fa' : 'far'} fa-star`} />
+          <i className={`${averageRating >= 4 ? 'fa' : 'far'} fa-star`} />
+          <i className={`${averageRating >= 5 ? 'fa' : 'far'} fa-star`} />
+                  </div>
+                  <p style={{ margin: '6% 0' }}></p>
+                  <p className="recommend" style={{ margin: '1% 0' }}>RECOMMENDED BY</p>
+                  <div className="recommended">
+                    <img src="img/recommended/user1.png" />
+                    <img src="img/recommended/user2.png" />
+                    <img src="img/recommended/user3.png" />
+                  </div>
+                </div>
+              </div>
             </div>
-        )
-    }
+            <div className="cover-info">
+              <p className="book-title">{title}</p>
+              <p className="book-author">{author}</p>
+              <div className="star-rating rate">
+              <i className={`${averageRating >= 1 ? 'fa' : 'far'} fa-star`} />
+          <i className={`${averageRating >= 2 ? 'fa' : 'far'} fa-star`} />
+          <i className={`${averageRating >= 3 ? 'fa' : 'far'} fa-star`} />
+          <i className={`${averageRating >= 4 ? 'fa' : 'far'} fa-star`} />
+          <i className={`${averageRating >= 5 ? 'fa' : 'far'} fa-star`} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 Book.propTypes = {
-    title: PropTypes.string,
-    author: PropTypes.string,
-    publishedDate: PropTypes.string,
-    description: PropTypes.string,
-    pageCount: PropTypes.string,
-    averageRating: PropTypes.number,
-    thumbnail: PropTypes.string,
-    id: PropTypes.string,
-  };
-  
-  Book.defaultProps = {
-    title: 'No title',
-    author: 'No author',
-    publishedDate: 'No published date',
-    description: 'No description',
-    pageCount: 'Not available',
-    averageRating: 0,
-    thumbnail: '',
-    id: '',
-  };
+  title: PropTypes.string,
+  author: PropTypes.string,
+  publishedDate: PropTypes.string,
+  description: PropTypes.string,
+  pageCount: PropTypes.string,
+  averageRating: PropTypes.number,
+  thumbnail: PropTypes.string,
+  id: PropTypes.string,
+};
+
+Book.defaultProps = {
+  title: 'No title',
+  author: 'No author',
+  publishedDate: 'No published date',
+  description: 'No description',
+  pageCount: 'Not available',
+  averageRating: 0,
+  thumbnail: '',
+  id: '',
+};
